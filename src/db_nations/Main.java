8291e8a13ae3c890ff64cbfdb68b3a156e7da49f
wilmer <wilmer.arango.a@gmail.com>
2023-11-03 18:26:01 +0100

    Mileston 3 🎉

--- src/db_nations/Main.java
+++ src/db_nations/Main.java
@@ -14,14 +14,20 @@ public class Main {
         try (Connection connection = DriverManager.getConnection(url,user, password)) {
             System.out.println(connection.getCatalog());
 
+            System.out.print("Search a country? ");
+            String countrySearch = scanner.nextLine();
+
             String query =
                     "select c.name as country_name , c.country_id , r.name as region_name , c2.name as continent_name "
                            + "from countries c "
                            + "join regions r on c.region_id = r.region_id "
                            + "join continents c2 on r.continent_id = c2.continent_id "
+                           + "where c.name like ? "
                            + "order by c.name;";
 
+
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)){
+                preparedStatement.setString(1, "%" + countrySearch + "%");
 
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()) {
@@ -31,7 +37,7 @@ public class Main {
                         String regionName = resultSet.getString("region_name");
                         String continentName = resultSet.getString("continent_name");
 
-                        System.out.println(continentName + " " + idCountry + " " + regionName + " " + countryName);
+                        System.out.println(countryName + " " + idCountry + " " + regionName + " " + continentName);
 
                      }
                 } catch (SQLException e) {
